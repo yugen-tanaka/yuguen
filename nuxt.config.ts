5da34d1203bbda3aa8467e31f99c672ddc488141
Yugen <yugen.tanaka@icloud.com>
2025-08-08 10:52:27 +0900

    nuxt iconを追加。

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon'],
   tailwindcss: {
     exposeConfig: true,
     viewer: true,
