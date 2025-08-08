b258ef714226c1668476d46f4802379753ee6937
Yugen <yugen.tanaka@icloud.com>
2025-08-08 11:40:15 +0900

    Vercel Speet Insightsを追加

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
+    <NuxtLayout>
     <NuxtRouteAnnouncer />
     <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { SpeedInsights } from '@vercel/speed-insights/vue';
+</script>
+
+<template>
+  <div>
+    <slot />
+
+    <SpeedInsights />
+  </div>
+</template>
