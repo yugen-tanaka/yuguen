546d2c8e24fcaaa6a71722042dd5d818ede180c9
Yugen <yugen.tanaka@icloud.com>
2025-08-09 08:08:06 +0900

    該当リリース情報を取得できる

--- app/pages/discography/[id].vue
+++ app/pages/discography/[id].vue
@@ -1,7 +1,24 @@
 <template>
+    <div class="relative min-h-screen bg-cover bg-center blur-xl" :style="{ backgroundImage: `url(/images/artworks/${release.id}.webp)` }">
+  <div class="absolute inset-0 bg-black bg-opacity-60 ">
+    
+    
+  </div>
+  
+  </div>
 
 </template>
 
 <script setup>
+import discographyData from '../data/discography.json';
+const route = useRoute();
+const router = useRouter();
 
+// URLのidパラメータから該当するリリースを検索
+const release = discographyData.find(r => r.id === route.params.id);
+
+// SmartLinkUrlがある場合はリダイレクトする
+if (release && release.url) {
+  router.replace(release.url);
+}
 </script>
