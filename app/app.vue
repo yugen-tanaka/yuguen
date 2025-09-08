108b45b182acd413c274511bbb4a33ca38c900e2
Yugen <yugen.tanaka@icloud.com>
2025-09-09 08:25:11 +0900

    Vercel Analyticsを追加

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,10 @@
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt';
+</script>
 <template>
     <NuxtLayout>
       <NuxtRouteAnnouncer />
+      <Analytics />
       <NuxtPage />
     </NuxtLayout>
 </template>
