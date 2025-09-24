8d5e27e9e71d7dd9573e1b1a29071e8ff15fd1fb
Yugen <yugen.tanaka@icloud.com>
2025-09-24 16:53:14 +0900

    Adding Nuxt Image module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon', '@nuxt/image'],
+  image: {
+
+  },
   tailwindcss: {
     exposeConfig: true,
     viewer: true,
